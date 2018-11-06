362a476d3d27332748161aa41a6f9a3013842229
Mauro Nunes <mauraoemau@hotmail.com>
2018-11-05 23:32:18 -0200

    first commit

---
+++ app/src/main/java/br/com/mnweb/myfisrtkotlin/MainActivity.kt
@@ -0,0 +1,19 @@
+package br.com.mnweb.myfisrtkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun change(view: View)
+    {
+        imageView.setImageResource(R.drawable.metallica2)
+    }
+}
